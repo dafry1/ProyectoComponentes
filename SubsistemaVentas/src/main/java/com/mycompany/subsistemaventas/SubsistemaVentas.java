2475086ef0d1996b0e013efe17a9b9124a488836
AndreVegaR <abelardo.vega262772@potros.itson.edu.mx>
2026-04-19 17:47:33 -0700

    Creé SubsistemaVentas pero no tiene nada xddddd. También mejoré lo que muestra InfoPieza, y ahora ahí puedes decidir la cantidad (con vali- daciones) para que se cree el DetallesVentaDTO (nueva clase creada), y estos detalles se ven en el carrito a la derecha en tarjetas, justo como se ve la selección de piezas

---
+++ SubsistemaVentas/src/main/java/com/mycompany/subsistemaventas/SubsistemaVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subsistemaventas;
+
+/**
+ *
+ * @author Andre
+ */
+public class SubsistemaVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
